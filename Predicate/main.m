5a4bb5054ad6d9ca2d092f738d12d0f90bf32ed2
shejunzhou <965678322@qq.com>
2015-11-28 23:00:22 +0800

    Initial Commit

---
+++ Predicate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Predicate
+//
+//  Created by shejun.zhou on 15/11/28.
+//  Copyright © 2015年 shejun.zhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
